eeae8a6567e2d7983394b862e7b573515fd20f9e
MichaelGloessl04 <gloess190117@gmail.com>
2024-03-23 00:54:49 +0100

    created basic listing of rooms

---
+++ src/components/ListElement.vue
@@ -0,0 +1,19 @@
+<template>
+    <tr>
+        <td>{{ props.room.id }}</td>
+        <td>{{ props.room.name }}</td>
+        <td>{{ props.room.occupied }}</td>
+    </tr>
+</template>
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { Room } from '../../types/Room';
+
+const props = defineProps<{
+    room: Room;
+}>();
+
+console.log(props);
+</script>
---
+++ src/components/RoomList.vue
@@ -0,0 +1,25 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Occupied</th>
+            </tr>
+        </thead>
+        <tbody>
+            <ListElement v-for="room in props.rooms" :room="room" />
+        </tbody>
+    </table>
+</template>
+
+
+<script setup lang="ts">
+import ListElement from './ListElement.vue';
+import { defineProps } from 'vue';
+import { Room } from '../../types/Room';
+
+const props = defineProps<{
+    rooms: Room[];
+}>();
+</script>
